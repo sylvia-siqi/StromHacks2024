337ba9199ce20402c8e60d8989c4a51bd28ebf40
sylvia-siqi <68520937+sylvia-siqi@users.noreply.github.com>
2024-05-19 03:32:07 -0700

    html for login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "../App.css";
+import "../index.css";
+import catImg from '../img/black_cat.png';
 
 const Login = () => {
     const [input, setInput] = useState("");
@@ -22,18 +25,31 @@ const Login = () => {
 
     return (
         <div>
-            <h1>Login</h1>
-            <Link to="/home">Home</Link>
+             <br></br>
+             <br></br>
+             <br></br>
+            <div class= "flex-col center-align">
+                <img class="top-item" src={catImg}></img>
+                <h1 class="accent-font margin-s">FitCat</h1>
+                <p class="accent-font txt-2 margin-s">Hit your fitness goals with your virtual cat!</p>
+            </div>
 
-            <form>
-                <label htmlFor="userinput">Username:</label>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <form className="flex-col">
+                <label htmlFor="userinput">Enter your ID</label>
                 <input type="text" id="userinput" value={input}
                     onChange={e => setInput(e.target.value)} />
 
-                <button type="submit" 
+                <button  type="submit" 
                     onClick={handleSubmit}
-                >Login</button>
+                >LOGIN</button>
             </form>
+            <p>Or <Link to="/">create a new account</Link> and start raising your cat!</p>
+
+
         </div>
         );
   };
